77a4d16c037f55bbb17d45ab678ead59500803c4
Alex R <areniak147@hotmail.com>
2019-02-24 12:58:19 -0500

    created skeleton for app

---
+++ models/notes.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const NotesSchema = new Schema({
+
+  title: String,
+
+  body: String
+
+});
+
+const Notes = mongoose.model("Notes", NotesSchema);
+
+module.exports = Notes;
