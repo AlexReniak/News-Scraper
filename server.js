39fe2211a8de8f4c189538476f911b446d7e3ecb
Alex R <areniak147@hotmail.com>
2019-02-26 11:00:06 -0500

    trying again

--- server.js
+++ server.js
@@ -1,36 +1,49 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-// Require axios and cheerio to scrape website and use it's data
-const axios = require("axios");
-const cheerio = require("cheerio");
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
-// Require models
-const db = require("./models");
+// Require all models
+var db = require("./models");
 
-const PORT = 3000;
+var PORT = 3000;
 
-const app = express();
+// Initialize Express
+var app = express();
 
+// Configure middleware
+
+// Use morgan logger for logging requests
 app.use(logger("dev"));
-
+// Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Make public a static folder
 app.use(express.static("public"));
 
-// Connect to mongo databse
-mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
+// Routes
 
-// Route to scrape website for data
+// A GET route for scraping the echoJS website
 app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
   axios.get("http://www.echojs.com/").then(function (response) {
-    const $ = cheerio.load(response.data);
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
     const articleArr = [];
+    // Now, we grab every h2 within an article tag, and do the following:
     $("article h2").each(function (i, element) {
-      const result = {};
+      // Save an empty result object
+      var result = {};
 
+      // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children("a")
         .text();
@@ -38,8 +51,6 @@ app.get("/scrape", function (req, res) {
         .children("a")
         .attr("href");
 
-      console.log(result.title)
-      console.log(result.link)
       articleArr.push(result);
      
     });
@@ -54,39 +65,49 @@ app.get("/scrape", function (req, res) {
   });
 });
 
-// Get route to retrieve data from db
+// Route for getting all Articles from the db
 app.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
   db.Article.find({})
     .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
+      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Route for grabbing a specific article by id and populate it with it's note
+// Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function (req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
     .populate("note")
     .then(function (dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
+      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function (dbNote) {
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function (err) {
+      // If an error occurred, send it to the client
       res.json(err);
     });
 });
